f0de354e5ed7737bc633d56a615d202daa877864
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-07-29 22:34:04 +0530

    Added cards for showing Headlines

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,28 @@ import NewsIntem from "./NewsIntem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is in News.js component
-        <NewsIntem />
-        <NewsIntem />
-        <NewsIntem />
-        <NewsIntem />
+      <div className="container my-3">
+        <h1>Please Visit Our Top News Headlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsIntem
+              title="First Title"
+              description="This is the first headline news"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsIntem
+              title="Second Title"
+              description="This is the second headline news"
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsIntem
+              title="Third Title"
+              description="This is the third headline news"
+            />
+          </div>
+        </div>
       </div>
     );
   }

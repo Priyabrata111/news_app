e74cf21e408657b45af25690561aea5de80d4a1c
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-08-26 23:21:40 +0530

    Converted NewsItem class to NewsItem function

--- src/components/NewsIntem.js
+++ src/components/NewsIntem.js
@@ -1,47 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsIntem extends Component {
-  render() {
-    let { title, description, imgurl, newsurl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              postion: "absolute",
-              right: "0",
-              border: "2px solid white",
-            }}
+const NewsIntem = (props) => {
+  let { title, description, imgurl, newsurl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            postion: "absolute",
+            right: "0",
+            border: "2px solid white",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+
+        <img src={imgurl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsurl}
+            target="_blank"
+            className="btn btn-dark"
+            rel="noopener noreferrer"
           >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-
-          <img src={imgurl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "Unknown" : author} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              href={newsurl}
-              target="_blank"
-              className="btn btn-dark"
-              rel="noopener noreferrer"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsIntem;
